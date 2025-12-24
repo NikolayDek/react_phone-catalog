04b916d37a9712fbfb1295589bf8285d046ff973
nikolay <diedaemon@gmail.com>
2025-12-24 22:38:22 +0100

    Updates

---
+++ assets/ProductsList-DrgpOSg1.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-8XUqOLd-.js";import{a as i}from"./ProductCard-C9K_dNPK.js";const d=({products:r})=>s.jsx("div",{className:"products-list",children:r.map(t=>s.jsx("div",{className:"products-list__item",children:s.jsx(i,{discount:!0,product:t})},t.itemId))});export{d as P};
